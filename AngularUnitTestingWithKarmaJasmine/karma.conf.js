b17bb9b57e66471dddaba37025acbc3ee8b95c9a
Rudrasish <rudrapanda117@gmail.com>
2017-10-22 00:58:34 +0530

    Code coverage for AngularUnitTestingWithKarmaJasmine project

--- AngularUnitTestingWithKarmaJasmine/karma.conf.js
+++ AngularUnitTestingWithKarmaJasmine/karma.conf.js
@@ -41,13 +41,13 @@ module.exports = function(config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: { './app/**/*.js': ['coverage'] },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['spec'],
+        reporters: ['progress', 'coverage'],
 
 
         // web server port
